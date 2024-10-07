abe70fb9cd8899debb3b654b5935c228a70d4618
junaama <0naama0@gmail.com>
2024-10-07 12:30:27 -0400

    Initial Commit

---
+++ fashion-avatar/fashion_avatarApp.swift
@@ -0,0 +1,17 @@
+//
+//  fashion_avatarApp.swift
+//  fashion-avatar
+//
+//  Created by macbook on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct fashion_avatarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
